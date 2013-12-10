5a8d89407b230cd644d72f9298b4136a185a8c2e
azumakuniyuki <azumakuniyuki@gmail.com>
2013-12-10 19:24:29 +0900

    Save the contents of configuration file into stash

--- lib/Noraneko/Web/User/Root.pm
+++ lib/Noraneko/Web/User/Root.pm
@@ -6,6 +6,7 @@ sub index {
     my $self = shift;
     my $lang = $self->stash('lang');
 
+    $self->stash( 'v' => $self->app->config->{'site'}->{ $lang } );
     return $self->render( 'template' => $lang.'/user/root/index' );
 }
 
