fcdf7ff9658732c380abec1e3adb17aa1b9e6b4c
azumakuniyuki <azumakuniyuki@gmail.com>
2013-12-11 16:10:20 +0900

    Add code for fallback loading configuration files

--- lib/Noraneko/Web.pm
+++ lib/Noraneko/Web.pm
@@ -1,21 +1,43 @@
 package Noraneko::Web;
 use Mojo::Base 'Mojolicious';
 use Path::Class;
+use Noraneko::Config;
 
 sub startup {
     my $self = shift;
     my $home = new Path::Class::File(__FILE__);
     my $root = $home->dir->resolve->absolute->parent->parent();
+    my $conf = {};
+    my $fail = 0;
 
     for my $e ( 'neko', 'auth' ) {
         my $f = $root->stringify.'/etc/'.$e.'.conf';
-        $self->plugin( 'Config', { 'file' => $f } );
+
+        eval {
+            $conf->{ $e } = $self->plugin( 'Config', { 'file' => $f } );
+        };
+        $conf->{ $e } ||= Noraneko::Config->$e;
+        $fail = 1 if $@;
     }
+
+    for my $e ( values %$conf ) {
+        while( my( $k, $v ) = each %$e ) {
+            $self->defaults->{'config'}->{ $k } = $v;
+        }
+    }
+    $self->defaults->{'config'}->{'site'}->{'maintenance'} = $fail;
+    $self->defaults->{'config'}->{'site'}->{'maintenance'} = 1;
 
     my $r = $self->routes;
 
     my $ctrl = undef;
     my $lang = [ 'ja', 'en' ];
+
+    if( $self->defaults->{'config'}->{'site'}->{'maintenance'} == 1 ) {
+        $ctrl = 'admin-maintenance#index';
+        $r->route( '/:lang/maintenance', 'lang' => $lang )->to( $ctrl );
+        $r->route( '/:lang/:any', 'lang' => $lang, 'any' => qr/.*/ )->to( $ctrl );
+    }
 
     $r->route( '/:lang/user/index', 'lang' => $lang )->to( 'user-root#index' );
 
